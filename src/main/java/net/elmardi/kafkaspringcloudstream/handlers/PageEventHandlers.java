86e19205be5a9c3d3436b5ca2f62553ee9b2c2df
achraf00007 <achrafelmardi473@gmail.com>
2025-05-18 18:09:55 +0100

    first commit

---
+++ src/main/java/net/elmardi/kafkaspringcloudstream/handlers/PageEventHandlers.java
@@ -0,0 +1,34 @@
+package net.elmardi.kafkaspringcloudstream.handlers;
+
+import net.elmardi.kafkaspringcloudstream.events.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Component
+public class PageEventHandlers {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (input)->{
+            System.out.println("-----------------------------");
+            System.out.println(input.toString());
+            System.out.println("-----------------------------");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(1000)
+            );
+        };
+    }
+}
