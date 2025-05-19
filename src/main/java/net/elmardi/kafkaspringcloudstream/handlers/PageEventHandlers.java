52d8914f3881c68dc5356bd01fa033ccb14eec0b
achraf00007 <achrafelmardi473@gmail.com>
2025-05-19 14:25:33 +0100

    second commit

--- src/main/java/net/elmardi/kafkaspringcloudstream/handlers/PageEventHandlers.java
+++ src/main/java/net/elmardi/kafkaspringcloudstream/handlers/PageEventHandlers.java
@@ -1,12 +1,21 @@
 package net.elmardi.kafkaspringcloudstream.handlers;
 
 import net.elmardi.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.metrics.stats.Avg;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -14,12 +23,11 @@ public class PageEventHandlers {
     @Bean
     public Consumer<PageEvent> pageEventConsumer(){
         return (input)->{
-            System.out.println("-----------------------------");
+            System.out.println("************");
             System.out.println(input.toString());
-            System.out.println("-----------------------------");
+            System.out.println("************");
         };
     }
-
     @Bean
     public Supplier<PageEvent> pageEventSupplier(){
         return ()->{
@@ -27,8 +35,23 @@ public class PageEventHandlers {
                     Math.random()>0.5?"P1":"P2",
                     Math.random()>0.5?"U1":"U2",
                     new Date(),
-                    10+new Random().nextInt(1000)
+                    10+new Random().nextInt(10000)
             );
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStream(){
+        return (input)->
+                input
+                        //.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(), v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        .count(Materialized.as("count-store"))
+                        .toStream()
+                        .map((k,v)->new KeyValue<>(k.key(), v))
+                ;
+
     }
 }
